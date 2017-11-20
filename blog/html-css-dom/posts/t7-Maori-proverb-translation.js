d46c61be60f18d7017fe9b5a9279e3565d6112ea
madeleine-brighouse <31881309+Maddiebrighouse@users.noreply.github.com>
2017-11-20 14:04:15 +1300

    Maori provber page added

---
+++ blog/html-css-dom/posts/t7-Maori-proverb-translation.js
@@ -0,0 +1,34 @@
+window.onload = function (){
+  document.getElementById("ajaxButton").addEventListener('click', makeRequest);
+}
+makeRequest()
+function makeRequest(){
+  var httpRequest = new XMLHttpRequest();
+  if (!httpRequest){
+    alert('Can not create XMLHTTP instace');
+    return false;
+  }
+
+  httpRequest.onreadystatechange = alertContents;
+  httpRequest.open('GET','https://eda-te-reo.herokuapp.com/api/proverbs', true);
+  httpRequest.send()
+
+  function alertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE){
+      if (httpRequest.status === 200) {
+        //var quotes = document.getElementById("quotes");
+        //quotes.innerHTML = httpRequest.responseText;
+        var quotes = JSON.parse(this.responseText);
+        //showQuotes(quotes)
+        document.getElementById("maori").innerHTML = quotes.source;
+        document.getElementById("english").innerHTML = quotes.translation;
+
+        quotes.innerHTML = httpRequest.responseText;
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
+
+
+}
